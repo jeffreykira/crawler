95ba831d2db20fd2f885142e403b8fb2af97d590
jeffreykira <jeffrey77918@gmail.com>
2019-06-24 23:39:17 +0800

    modify crawler script

--- crawler.py
+++ crawler.py
@@ -11,22 +11,20 @@ from icrawler.builtin import GoogleImageCrawler, FlickrImageCrawler
 logging.config.dictConfig(logging_config.DEV)
 log = logging.getLogger(__name__)
 
-CSV_FOLDER = '{}/classification_csv'.format(os.getcwd())
+CSV_FILE = '{}/classification.csv'.format(os.getcwd())
 IMAGE_FOLDER = '{}/classification_image'.format(os.getcwd())
 USERDATA_CONF = '{}/userdata.conf'.format(os.getcwd())
 FLICKR_KEY = ''
 
 
-def csv_parser(csv_path):
-    with open(csv_path, newline='') as csv_file:
+def csv_parser(csv_file):
+    with open(csv_file, newline='') as csv_file:
         try:
             dialect = csv.Sniffer().sniff(csv_file.read())
             csv_file.seek(0)
+            reader = csv.reader(csv_file, dialect)
         except csv.Error as e:
-            raise('csv file ({}) format error: {}'.format(csv_path, e))
-
-        reader = csv.reader(csv_file, dialect)
-        next(reader)
+            raise Exception('can not parse csv file, error: {}'.format(e))
 
         result = list()
         for row in reader:
@@ -53,7 +51,10 @@ def flickr_crawler(keyword, classification_folder):
 
 
 def download_image(keywords):
-    for k in keywords:  # keywords: [['食物', '巧克力醬'], ['食物', '貝果']]
+    if not os.path.exists(IMAGE_FOLDER):
+        os.mkdir(IMAGE_FOLDER)
+
+    for k in keywords:  # keywords: [['food', 'chocolate'], ['food', 'bagel']]
         if not k[0] or not k[1]:
             continue
 
@@ -68,24 +69,17 @@ def download_image(keywords):
 
 def crawler():
     log.debug('crawler start.')
-    if not os.listdir(CSV_FOLDER):
-        log.debug('classification_csv folder can not find csv file')
-        return
 
     config = configparser.ConfigParser()
     config.read(USERDATA_CONF)
     global FLICKR_KEY
     FLICKR_KEY = config['System']['FlickrKey']
 
-    for f in os.listdir(CSV_FOLDER):
-        file_name = os.path.splitext(f)
-        if file_name[1] != '.csv':
-            log.warning('file name: {}, not a csv file'.format(f))
-            continue
+    if not os.path.exists(CSV_FILE):
+        raise Exception('classification.csv file not found')
 
-        csv_path = '{}/{}'.format(CSV_FOLDER, f)
-        keywords = csv_parser(csv_path)
-        download_image(keywords)
+    keywords = csv_parser(CSV_FILE)
+    download_image(keywords)
 
     log.debug('crawler done.')
 
